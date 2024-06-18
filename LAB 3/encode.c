e283020187e2dc3e8d0477bb02b1466997056458
Lior Rosenberg <112342433+liorosen@users.noreply.github.com>
2024-06-18 18:25:53 +0000

    first trying

---
+++ LAB 3/encode.c
@@ -0,0 +1,111 @@
+%define SYS_READ 0x3
+%define SYS_WRITE 0x4
+%define SYS_OPEN 0x5
+%define SYS_CLOSE 0x6
+%define SYS_EXIT 0x1
+
+%define O_RDONLY 0x0
+%define O_WRONLY 0x1
+%define O_CREAT 0x40
+%define O_TRUNC 0x200
+
+section .bss
+inFile resd 1
+outFile resd 1
+char resb 1
+
+section .data
+newline db 0xA
+
+section .text
+global main
+extern strlen
+
+main:
+    push ebp
+    mov ebp, esp
+    mov eax, [ebp+8]        ; argc
+    mov ebx, [ebp+12]       ; argv
+
+    mov dword [inFile], 0   ; Initialize inFile to 0 (stdin)
+    mov dword [outFile], 1  ; Initialize outFile to 1 (stdout)
+
+    sub eax, 1              ; Decrement argc
+    jz encode               ; If no more arguments, start encoding
+    add ebx, 4              ; Move to the next argument
+
+scan_arguments:
+    mov ecx, [ebx]          ; Load current argument
+    cmp byte [ecx], '-'     ; Check if argument starts with '-'
+    jne next_arg
+    cmp byte [ecx+1], 'i'
+    je open_input_file
+    cmp byte [ecx+1], 'o'
+    je open_output_file
+next_arg:
+    sub eax, 1              ; Decrement argc
+    jz encode               ; If no more arguments, start encoding
+    add ebx, 4              ; Move to the next argument
+    jmp parse_args
+
+open_input_file:
+    call skip_and_open
+    mov ecx, O_RDONLY       ; O_RDONLY
+    int 0x80                ; Open the file
+    test eax, eax
+    js error_exit
+    mov dword [inFile], eax ; Save the file descriptor to inFile
+    jmp next_arg
+
+open_output_file:
+    call skip_and_open
+    mov ecx, O_WRONLY       ; O_WRONLY
+    or ecx, O_CREAT         ; O_CREAT
+    or ecx, O_TRUNC         ; O_TRUNC
+    mov edx, 0644o          ; File permissions
+    int 0x80                ; Open the file
+    test eax, eax
+    js error_exit
+    mov dword [outFile], eax ; Save the file descriptor to outFile
+    jmp next_arg
+
+encode:
+    pushad                  ; Save all general-purpose registers
+read_char:
+    mov eax, SYS_READ       ; sys_read
+    mov ebx, [inFile]       ; stdin or input file
+    lea ecx, [char]         ; Buffer to store character
+    mov edx, 1              ; Read one byte
+    int 0x80
+    cmp eax, 0              ; Check if end of file
+    jle done            
+    ; Encoding the character part:
+    cmp byte [char], 'A'
+    jl print_char
+    cmp byte [char], 'z'
+    jg print_char
+    add byte [char], 1
+print_char:
+    mov eax, SYS_WRITE      ; sys_write
+    mov ebx, [outFile]      ; stdout or output file
+    lea ecx, [char]         ; Character to write
+    mov edx, 1              ; Write one byte
+    int 0x80
+    jmp read_char
+
+done:
+    popad                   ; Restore all general-purpose registers
+    pop ebp
+    ret
+
+skip_and_open:
+    add ecx, 2              ; Skip "-o" or "-i"
+    mov eax, SYS_OPEN       ; sys_open
+    mov ebx, ecx            ; File name
+    ret
+
+error_exit:
+    mov eax, SYS_EXIT       ; sys_exit
+    mov ebx, 0x55           ; exit code
+    int 0x80                ; Make the system call to exit
+    ret
