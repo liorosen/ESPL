b426d4af0c7ab8219a9def8848bedb2a8a6d2c16
Lior Rosenberg <112342433+liorosen@users.noreply.github.com>
2024-07-07 06:48:53 +0000

    3

--- LAB 5/Task 3/myElf.c
+++ LAB 5/Task 3/myElf.c
@@ -10,7 +10,8 @@
 
 typedef struct {
     char debug_mode;
-    char file_name[128];
+    char file_name1[128]; // Added to store file name for fd1
+    char file_name2[128]; // Added to store file name for fd2
     int unit_size;
     unsigned char mem_buf[10000];
     size_t mem_count;
@@ -23,7 +24,7 @@ typedef struct {
     size_t file_size2;
 } state;
 
-state s = {0, "", 1, {0}, 0, 0, -1, -1, NULL, NULL, 0, 0}; // Initializing all the struct paramaters by order of creation.
+state s = {0, "", "", 1, {0}, 0, 0, -1, -1, NULL, NULL, 0, 0}; // Initializing all struct parameters by order of creation.
 
 // Switches the debug mode on or off
 void toggle_debug_mode(state* s) {
@@ -85,21 +86,19 @@ bool map_and_validate_elf_file(int fd, struct stat sb, void** map_start) {
     return true;
 }
 
-void errChecks(int fd, struct stat *sb) {
+bool err_checks(int fd, struct stat *sb) {
     if (fd == -1) {
         perror("Error opening file");
-        return;
+        return false;
     }
 
-    // Initialize the struct stat object before using it
     memset(sb, 0, sizeof(struct stat));
-    
-    // Get information about the file referred to by the file descriptor fd
     if (fstat(fd, sb) == -1) {
         perror("Error getting file size");
         close(fd);
-        return;
+        return false;
     }
+    return true;
 }
 
 void examine_elf_file(state* s) {
@@ -118,24 +117,22 @@ void examine_elf_file(state* s) {
     file_name[strcspn(file_name, "\n")] = '\0';
 
     fd = open(file_name, O_RDONLY);
-    errChecks( fd, &sb);
-
-    map_start = mmap(NULL, sb.st_size, PROT_READ, MAP_PRIVATE, fd, 0);
-    if (map_start == MAP_FAILED) {
-        perror("Error mapping file");
-        close(fd);
+    if (!err_checks(fd, &sb)) {
+        printf("Error opening file: %s\n", file_name);
+        return;
+    }
+    if (!map_and_validate_elf_file(fd, sb, &map_start)) {
         return;
     }
 
     Elf32_Ehdr* header = (Elf32_Ehdr*) map_start;
-    if (memcmp(header->e_ident, ELFMAG, SELFMAG) != 0) {
-        printf("Not an ELF file\n");
-        munmap(map_start, sb.st_size);
-        close(fd);
-        return;
-    }
 
     mangagingMultipleFiles(s, fd, sb, map_start);
+    if (fd == s->fd1) {
+        strcpy(s->file_name1, file_name); // Store file name for fd1
+    } else if (fd == s->fd2) {
+        strcpy(s->file_name2, file_name); // Store file name for fd2
+    }
     print_elf_header(header);
 }
 
@@ -166,12 +163,14 @@ void interpretELFfile(int fd, state* s, Elf32_Ehdr** header_out, Elf32_Shdr** se
         *section_headers_out = (Elf32_Shdr*) (s->map_start1 + (*header_out)->e_shoff);
         // This line sets *strtab_out to point to the start of the section header string table, allowing access to the names of the sections in the ELF file.
         *strtab_out = (const char*) (s->map_start1 + (*section_headers_out)[(*header_out)->e_shstrndx].sh_offset);
+        printf("File: %s\n", s->file_name1);
     } else if (fd == 2) {
         *header_out = (Elf32_Ehdr*) s->map_start2;
         *section_headers_out = (Elf32_Shdr*) (s->map_start2 + (*header_out)->e_shoff);
         *strtab_out = (const char*) (s->map_start2 + (*section_headers_out)[(*header_out)->e_shstrndx].sh_offset);
+        printf("File: %s\n", s->file_name2);
     }
-    printf("File: %s\n", s->file_name);
+
 }
 
 void print_section_names_helper(int fd, state* s) {
@@ -191,7 +190,6 @@ void print_section_names_helper(int fd, state* s) {
     }
 }
 
-
 void print_section_names(state* s) {
     if (s->fd1 == -1 && s->fd2 == -1) {
         printf("No ELF files are currently examined\n");
@@ -207,7 +205,10 @@ void print_section_names(state* s) {
     }
 }
 
-void print_symbols_for_file_helper(const Elf32_Ehdr* header, const Elf32_Shdr* section_headers, const char* strtab, const Elf32_Sym* symtab, const char* symtab_strtab, const Elf32_Shdr* symtab_section, state* s) {
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+// Print symbols functions
+void print_symbols_for_file_helper(const Elf32_Ehdr* header, const Elf32_Shdr* section_headers, const char* strtab, const Elf32_Sym* symtab, const char* symtab_strtab, const Elf32_Shdr* symtab_section, state* s, int fd) {
     int num_symbols = symtab_section->sh_size / symtab_section->sh_entsize;
 
     if (s->debug_mode) {
@@ -215,7 +216,13 @@ void print_symbols_for_file_helper(const Elf32_Ehdr* header, const Elf32_Shdr* s
         printf("Number of symbols: %d\n", num_symbols);
     }
 
-    printf("File: %s\n", s->file_name);
+    /*if (fd == 1) {
+        printf("File: %s\n", s->file_name1);
+    } else if (fd == 2) {
+        printf("File: %s\n", s->file_name2);
+    }*/
+
+    printf("File: %s\n", fd == 1 ? s->file_name1 : s->file_name2);
     for (int i = 0; i < num_symbols; i++) {
         const Elf32_Sym* sym = &symtab[i];
         const char* sym_name = symtab_strtab + sym->st_name; // Get the symbol name
@@ -224,7 +231,6 @@ void print_symbols_for_file_helper(const Elf32_Ehdr* header, const Elf32_Shdr* s
         printf("[%d] %08x %d %s %s\n", i, sym->st_value, sym->st_shndx, sec_name, sym_name);
     }
 }
-
 
 void print_symbols_for_file(int fd, state* s) {
     // Referenced from here : https://stackoverflow.com/questions/17029206/searching-init-section-in-elf-file
@@ -255,9 +261,8 @@ void print_symbols_for_file(int fd, state* s) {
     // Calculates the address of the string table used by the symbol table, in order to get the exact memory location of the string table.
     const char* symtab_strtab = (const char*) (address_file + section_headers[symtab_section->sh_link].sh_offset);
 
-    print_symbols_for_file_helper(header, section_headers, strtab, symtab, symtab_strtab, symtab_section, s);
+    print_symbols_for_file_helper(header, section_headers, strtab, symtab, symtab_strtab, symtab_section, s, fd);
 }
-
 
 void print_symbols(state* s) {
     if (s->fd1 == -1 && s->fd2 == -1) {
@@ -274,27 +279,9 @@ void print_symbols(state* s) {
     }
 }
 
-
-void stub_function(state* s) {
-    printf("Not implemented yet\n");
-}
-
-void quit(state* s) {
-    if (s->fd1 != -1) {
-        munmap(s->map_start1, s->file_size1);
-        close(s->fd1);
-    }
-
-    if (s->fd2 != -1) {
-        munmap(s->map_start2, s->file_size2);
-        close(s->fd2);
-    }
-
-    if (s->debug_mode) {
-        fprintf(stderr, "quitting\n");
-    }
-    exit(0);
-}
+// void stub_function(state* s) {
+//     printf("Not implemented yet\n");
+// }
 
 void find_Symbol_Table_Sections(const Elf32_Ehdr* header1, const Elf32_Ehdr* header2, const Elf32_Shdr* section_headers1, const Elf32_Shdr* section_headers2, Elf32_Shdr** symtab_section1, Elf32_Shdr** symtab_section2) {
     *symtab_section1 = NULL;
@@ -340,35 +327,47 @@ void compareSymbols(int num_symbols1, int num_symbols2, Elf32_Sym* symtab1, Elf3
     }
 }
 
-
-void check_merge(state* s) {
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+// Merge files functions
+bool check_merge_helper(state* s, Elf32_Shdr** symtab_section1, Elf32_Shdr** symtab_section2, Elf32_Shdr** section_headers1, Elf32_Shdr** section_headers2, const char** strtab1, const char** strtab2) {
     if (s->fd1 == -1 || s->fd2 == -1) {
         printf("Two ELF files must be opened and mapped.\n");
-        return;
+        return false;
     }
 
     // Get headers and section headers for both files
     Elf32_Ehdr* header1;
-    Elf32_Shdr* section_headers1;
-    const char* strtab1;
-    interpretELFfile(1, s, &header1, &section_headers1, &strtab1);
+    interpretELFfile(1, s, &header1, section_headers1, strtab1);
 
     Elf32_Ehdr* header2;
-    Elf32_Shdr* section_headers2;
-    const char* strtab2;
-    interpretELFfile(2, s, &header2, &section_headers2, &strtab2);
+    interpretELFfile(2, s, &header2, section_headers2, strtab2);
 
-    // Define pointers to store the symbol table sections
-    Elf32_Shdr* symtab_section1;
-    Elf32_Shdr* symtab_section2;
-
-    find_Symbol_Table_Sections(header1, header2, section_headers1, section_headers2, &symtab_section1, &symtab_section2);
+    // Find symbol table sections
+    find_Symbol_Table_Sections(header1, header2, *section_headers1, *section_headers2, symtab_section1, symtab_section2);
 
     // Check if symbol tables are found in both files
-    if (!symtab_section1 || !symtab_section2) {
+    if (!*symtab_section1 || !*symtab_section2) {
         printf("Feature not supported: Each file must contain exactly one symbol table.\n");
+        return false;
+    }
+
+    return true;
+}
+
+void check_merge(state* s) {
+    Elf32_Shdr* symtab_section1;
+    Elf32_Shdr* symtab_section2;
+    Elf32_Shdr* section_headers1;
+    Elf32_Shdr* section_headers2;
+    const char* strtab1;
+    const char* strtab2;
+
+    if (!check_merge_helper(s, &symtab_section1, &symtab_section2, &section_headers1, &section_headers2, &strtab1, &strtab2)) {
         return;
     }
+
+    printf("File: %s\n", s->file_name1); // Print file name for fd1
+    printf("File: %s\n", s->file_name2); // Print file name for fd2
 
     void* address_file1 = s->map_start1;
     Elf32_Sym* symtab1 = (Elf32_Sym*) (address_file1 + symtab_section1->sh_offset);
@@ -385,6 +384,100 @@ void check_merge(state* s) {
 }
 
 
+void sections_Merging(state* s, Elf32_Ehdr* header1, const char* strtab1, Elf32_Shdr* symtab_section1, Elf32_Shdr* symtab_section2, Elf32_Shdr* section_headers1, Elf32_Shdr* section_headers2, size_t offset, Elf32_Shdr* new_section_headers, FILE* out_file, const char* merge_sections[], int merge_sections_length) {
+    for (int i = 0; i < header1->e_shnum; i++) {
+        const char* section_name = strtab1 + section_headers1[i].sh_name;
+
+        // These sections do not need to be merged because they are self-contained and do not depend on the content of other sections.
+        if (strcmp(section_name, ".shstrtab") == 0 || strcmp(section_name, ".symtab") == 0 || strcmp(section_name, ".strtab") == 0) {
+            // Copy these sections directly without merging
+            fwrite((char*)s->map_start1 + section_headers1[i].sh_offset, 1, section_headers1[i].sh_size, out_file);  // Writes the section data from the first ELF file to the output file.
+            new_section_headers[i].sh_offset = offset;
+            offset += section_headers1[i].sh_size;    // Updates the offset for the next section.
+        } else {
+            // Merge sections
+            for (int j = 0; j < merge_sections_length; j++) {
+                if (strcmp(section_name, merge_sections[j]) == 0) {
+                    fwrite((char*)s->map_start1 + section_headers1[i].sh_offset, 1, section_headers1[i].sh_size, out_file);
+                    fwrite((char*)s->map_start2 + section_headers2[i].sh_offset, 1, section_headers2[i].sh_size, out_file);
+                    new_section_headers[i].sh_size += section_headers2[i].sh_size;
+                    new_section_headers[i].sh_offset = offset;
+                    offset += new_section_headers[i].sh_size;
+                    break;
+                }
+            }
+        }
+    }
+}
+
+void merge_elf_files(state* s) {
+    Elf32_Shdr* symtab_section1;
+    Elf32_Shdr* symtab_section2;
+    Elf32_Shdr* section_headers1;
+    Elf32_Shdr* section_headers2;
+    const char* strtab1 = NULL;
+    const char* strtab2 = NULL;
+
+    // Open the output file
+    FILE* out_file = fopen("out.ro", "wb");
+    if (!out_file) {
+        perror("Error opening output file");
+        return;
+    }
+
+    if (!check_merge_helper(s, &symtab_section1, &symtab_section2, &section_headers1, &section_headers2, &strtab1, &strtab2)) {
+        fclose(out_file);  // Close the output file if the merge check fails
+        return;
+    }
+
+    Elf32_Ehdr* header1 = (Elf32_Ehdr*) s->map_start1;
+
+    fwrite(header1, 1, sizeof(Elf32_Ehdr), out_file);
+
+    Elf32_Shdr* new_section_headers = malloc(header1->e_shnum * sizeof(Elf32_Shdr));      // Allocates memory for the new section header table specified by header1->e_shnum.
+    memcpy(new_section_headers, section_headers1, header1->e_shnum * sizeof(Elf32_Shdr)); // Copies the section header table
+
+    // Merge sections .text, .data, .rodata
+    const char* merge_sections[] = {".text", ".data", ".rodata"};
+    size_t offset = sizeof(Elf32_Ehdr); // Start offset after the ELF header
+
+    sections_Merging(s, header1, strtab1, symtab_section1, symtab_section2, section_headers1, section_headers2, offset, new_section_headers, out_file, merge_sections, sizeof(merge_sections) / sizeof(merge_sections[0]));
+
+    // Write the new section header table at the end of the file
+    long shoff = ftell(out_file);  // Gets the current file position, which will be the offset for the new section header table.
+    fwrite(new_section_headers, 1, header1->e_shnum * sizeof(Elf32_Shdr), out_file);
+
+    // Update the ELF header with the new section header table offset
+    header1->e_shoff = shoff;
+    fseek(out_file, 0, SEEK_SET);  // Repositions the file pointer to the beginning of the file.
+    fwrite(header1, 1, sizeof(Elf32_Ehdr), out_file);  // Writes the updated ELF header to the file.
+
+    // Cleanup
+    free(new_section_headers);
+    fclose(out_file);
+
+    printf("Merged ELF file created: out.ro\n");
+}
+
+/*------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------*/
+
+void quit(state* s) {
+    if (s->fd1 != -1) {
+        munmap(s->map_start1, s->file_size1);
+        close(s->fd1);
+    }
+
+    if (s->fd2 != -1) {
+        munmap(s->map_start2, s->file_size2);
+        close(s->fd2);
+    }
+
+    if (s->debug_mode) {
+        fprintf(stderr, "quitting\n");
+    }
+    exit(0);
+}
+
 
 typedef void (*menu_function)(state* s);
 
@@ -393,20 +486,19 @@ menu_function menu_functions[] = {
     examine_elf_file,
     print_section_names,
     print_symbols,
-    check_merge, 
-    stub_function, // Merge ELF Files
+    check_merge,
+    merge_elf_files, 
     quit,
     NULL
 };
-
 
 const char* menu[] = {
     "0-Toggle Debug Mode",
     "1-Examine ELF File",
     "2-Print Section Names",
-    "3-Print Symbols", 
+    "3-Print Symbols",
     "4-Check Files for Merge",
-    "5-Merge ELF Files",
+    "5-Merge ELF Files", 
     "6-Quit",
     NULL
 };
